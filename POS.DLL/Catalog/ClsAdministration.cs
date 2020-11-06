3fe59c2a4d4ae6a3714951fa806adf6d90f90f79
Israel González <igonzalez@laespanola.com.ec>
2020-11-06 00:10:25 +0000

    Avances Dll Imported from Changeset 29

---
+++ POS.DLL/Catalog/ClsAdministration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.DLL.Catalog
+{
+    public class ClsAdministration
+    {
+        public SP_Login_Consult_Result GetLoginInformation(
+                                                            string _user
+                                                            , string _password
+                                                            , string _workstation
+                                                            , string _addressIP
+                                                            )
+        {
+            var db = new POSEntities();
+            SP_Login_Consult_Result result;
+
+            try
+            {
+                result = db.SP_Login_Consult(
+                                            _user
+                                            , _password
+                                            , _workstation
+                                            , _addressIP
+                                            ).FirstOrDefault();
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return result;
+        }
+    }
+}
--- POS.DLL/UserLogin.cs
+++ POS.DLL/UserLogin.cs
@@ -12,11 +12,23 @@ namespace POS.DLL
     using System;
     using System.Collections.Generic;
     
-    public partial class Supervisor
+    public partial class UserLogin
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public UserLogin()
+        {
+            this.Supervisor = new HashSet<Supervisor>();
+        }
+    
         public int UserId { get; set; }
-        public int PasswordId { get; set; }
-        public string barcode { get; set; }
+        public string UserName { get; set; }
+        public string Lastname { get; set; }
+        public string Firtsname { get; set; }
+        public bool IsProfile { get; set; }
+        public bool IsAdministrator { get; set; }
+        public string Type { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
         public string Status { get; set; }
         public int CreatedBy { get; set; }
         public System.DateTime CreatedDatetime { get; set; }
@@ -24,6 +36,7 @@ namespace POS.DLL
         public Nullable<System.DateTime> ModifiedDatetime { get; set; }
         public string Workstation { get; set; }
     
-        public virtual UserLogin UserLogin { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Supervisor> Supervisor { get; set; }
     }
 }
