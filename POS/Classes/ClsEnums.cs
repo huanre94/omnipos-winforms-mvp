d011ae24fd486035f63e083c4d9e666e8e6cbc56
Israel González <igonzalez@laespanola.com.ec>
2020-11-06 15:18:44 +0000

    Asignacion de Id para PaymModeEnum Imported from Changeset 35

--- POS/Classes/ClsEnums.cs
+++ POS/Classes/ClsEnums.cs
@@ -18,20 +18,20 @@ namespace POS.Classes
 
         public enum PaymModeEnum
         {
-            BONO,
-            CHEQUE_DIA,
-            CHEQUE_POST,
-            CREDITO,
-            DEBITO_BANCARIO,
-            DEPOSITO_BANCARIO,
-            DINERO_ELECTRONICO,
-            EFECTIVO,
-            NOTA_CREDITO,
-            ANTICIPOS,
-            PAGOS_WEB,
-            RETENCION,
-            TARJETA_CREDITO,
-            TARJETA_CONSUMO
+            BONO = 1,
+            CHEQUE_DIA = 2,
+            CHEQUE_POST = 3,
+            CREDITO = 4,
+            DEBITO_BANCARIO = 5,
+            DEPOSITO_BANCARIO = 6,
+            DINERO_ELECTRONICO = 7,
+            EFECTIVO = 8,
+            NOTA_CREDITO = 9,
+            ANTICIPOS= 10,
+            PAGOS_WEB = 11,
+            RETENCION = 12,
+            TARJETA_CREDITO = 13,
+            TARJETA_CONSUMO = 14
         }
 
         public enum InputFromOption
