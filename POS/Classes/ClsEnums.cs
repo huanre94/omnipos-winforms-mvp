7d8130b876fda867dc195507b4f27db684981c67
Israel González <igonzalez@laespanola.com.ec>
2020-11-17 17:11:26 +0000

    Front End Imported from Changeset 71

--- POS/Classes/ClsEnums.cs
+++ POS/Classes/ClsEnums.cs
@@ -58,10 +58,10 @@
 
         public enum LogType
         {
-            Anular_Documento = 1,
-            Eliminar_Producto = 2,
-            Suspender_Documento = 3,
-            Recuperar_Documento = 4
+            ANULAR_DOCUMENTO = 1,
+            ELIMINAR_PRODUCTO = 2,
+            SUSPENDER_DOCUMENTO = 3,
+            RECUPERAR_DOCUMENTO = 4
         }
     }
 }
